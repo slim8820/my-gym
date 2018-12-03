87a087d995dfc68f80b321aaf45e05d76d7429c4
slim <44605803+slim8820@users.noreply.github.com>
2018-12-04 00:55:57 +0100

    Add files via upload
    ili bech tcopier menou

--- callbacks.c
+++ callbacks.c
@@ -1,92 +1,162 @@
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
-#include "fonction.h"
-#include <string.h>
 
 #include <gtk/gtk.h>
+#include <string.h>
 
 #include "callbacks.h"
 #include "interface.h"
 #include "support.h"
+#include "verfier.h"
+
+
+
+
+
+
+
 
 
 void
-on_button1_clicked                     (GtkWidget       *objet_graphique,
-                                        gpointer         user_data)
+on_button1_clicked                     (GtkWidget       *button , gpointer         user_data)
+
 {
+GtkWidget *window;
+GtkWidget *window1;
+GtkWidget *input;
+GtkWidget *input2;
+GtkWidget *output;
+char x[50];char y[50];
+char ext[50];
+int verif;
+ 
+window1=lookup_widget(button,"window1");
+input=lookup_widget(button,"entry1");
+strcpy(x,gtk_entry_get_text(GTK_ENTRY(input)));
+input2=lookup_widget(button,"entry2");
+strcpy(y,gtk_entry_get_text(GTK_ENTRY(input2)));
+verif=verifier(x,y);
+if( verif==1){
+gtk_widget_hide(window1);
+window=create_admin();
+gtk_widget_show_all(window);}
+else if( verif==2){
+gtk_widget_hide(window1);
+window=create_adherent();
+gtk_widget_show_all(window);}
+else if( verif==3){
+gtk_widget_hide(window1);
+window=create_coach();
+gtk_widget_show_all(window);}
+else if( verif==4){
+gtk_widget_hide(window1);
+window=create_nutritionniste();
+gtk_widget_show_all(window);}
+else if( verif==5){
+gtk_widget_hide(window1);
+window=create_dieteticien();
+gtk_widget_show_all(window);}
+else if( verif==6){
+gtk_widget_hide(window1);
+window=create_kine();
+gtk_widget_show_all(window);}
+else if( verif==-1){
+strcpy(ext,"erreur:user name/ password uncorrect");
+output=lookup_widget(button,"label8");
+gtk_label_set_text(GTK_LABEL(output),ext);}
+
+}
+void
+on_button2_clicked                     (GtkWidget       *button , gpointer         user_data)
+
+{
+ 
+GtkWidget *window;
+window=lookup_widget(button,"admin");
+gtk_widget_hide(window);
+window=create_ajouter();
+gtk_widget_show_all(window);
+
+}
+void
+on_button3_clicked                     (GtkWidget       *button , gpointer         user_data)
+
+{
+GtkWidget *window;
+GtkWidget *input3;
+GtkWidget *input4;
+GtkWidget *input5;
+int r2;
+char x[50];
+char y[50];
+FILE* f ;
+
+window=lookup_widget(button,"ajouter");
+input3=lookup_widget(button,"entry11");
+strcpy(x,gtk_entry_get_text(GTK_ENTRY(input3)));
+input4=lookup_widget(button,"entry12");
+strcpy(y,gtk_entry_get_text(GTK_ENTRY(input4)));
+input5=lookup_widget(button,"spinbutton1");
+r2=gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (input5));  
+f=fopen("/home/slimmensi/Projects/project3/src/users.txt","a++");
+if(f !=NULL) 
+	{
+		fprintf(f,"%s %s %d \n",x,y,r2);
+	}
+fclose(f);
+}
+
+
+
+void
+on_button4_clicked                     (GtkWidget       *button, gpointer  user_data)
+{
+
+
+users adm;
+GtkWidget *input1;
+GtkWidget *input2;
+char username[20];
+char password[20];
+
+input1=lookup_widget(button,"entry13");
+input2=lookup_widget(button,"entry14");
+strcpy(username,gtk_entry_get_text(GTK_ENTRY(input1)));
+strcpy(password,gtk_entry_get_text(GTK_ENTRY(input2)));
+ FILE* f;
+ FILE* f_temp;   
+int i=0;
+   f=fopen("/home/slimmensi/Projects/project3/src/users.txt","r");
+ f_temp=fopen("/home/slimmensi/Projects/project3/src/users_tmp.txt","a+");
+while(fscanf(f,"%s %s",Username,Password)!=EOF)
+   {i++;}
+fclose(f);
+
+f=fopen("admin.txt","r");
+ f_temp=fopen("/home/slimmensi/Projects/project3/src/users_tmp.txt","a+");
+
+   while(fscanf(f,"%s %s",username,password)!=EOF)
+   {  
+     if ((strcmp(username)!=0)||(strcmp(password)!=0))
+{
+fprintf(f_temp,"%s %s\n",username,password);
+  }
+};
+fclose(f);
+fclose(f_temp);
+remove("/home/slimmensi/Projects/project3/src/users.txt");
+rename("/home/slimmensi/Projects/project3/src/users_tmp.txt","users.txt");
+
+
 
 }
 
 
 void
-on_button3_clicked                     (GtkWidget       *objet_graphique,
+on_button5_clicked                     (GtkButton       *button,
                                         gpointer         user_data)
 {
 
 }
-
-
-void
-on_button4_clicked                     (GtkWidget       *objet_graphique,
-                                        gpointer         user_data)
-{
-
-}
-
-
-void
-on_button2_clicked                     (GtkWidget       *objet_graphique,
-                                        gpointer         user_data)
-{
-
-}
-
-
-void
-on_button5_clicked                     (GtkWidget       *objet_graphique,
-                                        gpointer         user_data)
-{
-
-GtkWidget *nom;
-GtkWidget *prenom;
-GtkWidget *age;
-GtkWidget *sexe;
-GtkWidget *poids;
-GtkWidget *taille;
-GtkWidget *cin;
-GtkWidget *password;
-
-GtkWidget *date_dabonnement;
-GtkWidget *jours;
-GtkWidget *mois;
-GtkWidget *annee;
-
-Date dt_nais;
-admin s ;
-jours=lookup_widget(objet_graphique,"spinbutton1");
-mois=lookup_widget(objet_graphique,"spinbutton2");
-annee=lookup_widget(objet_graphique,"spinbutton3");
-password=lookup_widget(objet_graphique,"entry9");
-cin=lookup_widget(objet_graphique,"entry10");
-nom=lookup_widget(objet_graphique,"entry3");
-prenom=lookup_widget(objet_graphique,"entry4");
-age=lookup_widget(objet_graphique,"entry5");
-sexe=lookup_widget(objet_graphique,"entry6");
-poids=lookup_widget(objet_graphique,"entry7");
-taille=lookup_widget(objet_graphique,"entry8");
-s.dt_nais.jours=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(jours));
-s.dt_nais.mois=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(mois));
-s.dt_nais.annee=gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(annee));
-strcpy(s.username,gtk_entry_get_text(GTK_ENTRY(cin)));
-strcpy(s.password,gtk_entry_get_text(GTK_ENTRY(password)));
-strcpy(s.nom,gtk_entry_get_text(GTK_ENTRY(nom)));
-strcpy(s.prenom,gtk_entry_get_text(GTK_ENTRY(prenom)));
-ajouter(s);
-}
-
-
-
-
-
 
